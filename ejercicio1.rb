9072f20f0451c669203d17eb6824eb5abff334eb
paskuvan <majopaskuvan@gmail.com>
2018-06-16 21:08:49 -0400

    resolved :sunglasses:

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,5 +1,25 @@
-a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6] #lista
 
-copia = a.map { |e| e +1  }
-puts copia
-puts a
+aumenta = a.each.map { |i| i + 1 } #aumentar en 1
+puts aumenta, "\n"
+
+convert_float = a.map(&:to_f) #convertir al float
+puts convert_float, "\n"
+
+convert_string = a.map(&:to_s) #convertir al string
+puts convert_string, "\n"
+
+menor = a.each.reject { |i| i < 5 } #menor a 5
+puts menor, "\n"
+
+mayor = a.each.select { |i| i < 5 } #mayor a 5
+puts mayor, "\n"
+
+suma = a.inject(0) { |sum, x| sum + x } #suma total
+puts suma, "\n"
+
+pares = a.group_by(&:even?) #son pares o no 
+puts pares, "\n"
+
+agrupar = a.group_by { |num| num > 6 } #agrupar los numeros mayores
+puts agrupar, "\n"
